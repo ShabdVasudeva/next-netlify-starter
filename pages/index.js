3931dc25c37b52ef8120748959c0bc14ca885f04
Shabd Vasudeva <127197800+developer2207@users.noreply.github.com>
2023-12-24 15:00:07 +0000

    jsj

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import 'styles/globals.css'
+import './styles/globals.css'
 
 export default function Home() {
   return (
